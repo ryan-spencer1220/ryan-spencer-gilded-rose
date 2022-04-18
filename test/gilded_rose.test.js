0f3db85b28c07001c81ba80b696abc0d3eacf4fa
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-04-18 06:57:26 -0700

    Pass initial test

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,9 +1,11 @@
 const { Shop, Item } = require("../src/gilded_rose");
 
 describe("Gilded Rose", function () {
-  it("should foo", function () {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+  it("should decrease sellIn by one", function () {
+    const gildedRose = new Shop([new Item("foo", -1, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+    expect(items[0].name).toBe("foo");
+    expect(items[0].sellIn).toBe(-2);
+    expect(items[0].quality).toBe(0);
   });
 });
