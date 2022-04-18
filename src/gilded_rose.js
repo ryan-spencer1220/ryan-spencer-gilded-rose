1c44a47fc772e1525ef6e4d39c8f8216e1d719cd
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-04-18 07:46:11 -0700

    Complete initial refactor

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -12,55 +12,42 @@ class Shop {
   }
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (
-        this.items[i].name != "Aged Brie" &&
-        this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
-      ) {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
+      // Sulfuras is the only item with a fixed quallity value& sellIn value
+      if (this.items[i].name == "Sulfuras, Hand of Ragnaros") {
+        // All other items have a sellIn value that decreases by one
       } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (
-            this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"
-          ) {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
+        this.items[i].sellIn -= 1;
+        // Aged Brie increases in quality value increases by one
+        if (this.items[i].name == "Aged Brie") {
+          this.items[i].quality += 1;
+          // Conjured items decrease in quality value by two
+        } else if (this.items[i].name.includes("Conjured")) {
+          this.items[i].quality -= 2;
+          // Backstage Passes fluctuate in quality value based on sellIn value
+        } else if (
+          this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"
+        ) {
+          if (this.items[i].sellIn >= 10) {
+            this.items[i].quality += 1;
+          } else if (this.items[i].sellIn < 10 && this.items[i].sellIn >= 5) {
+            this.items[i].quality += 2;
+          } else if (this.items[i].sellIn < 5 && this.items[i].sellIn >= 1) {
+            this.items[i].quality += 3;
+          } else if (this.items[i].sellIn <= 0) {
+            this.items[i].quality = 0;
           }
-        }
-      }
-      if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != "Aged Brie") {
-          if (
-            this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
-          ) {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality =
-              this.items[i].quality - this.items[i].quality;
-          }
+          // All other items with a sellIn value below zero decrease in quality value by two
+        } else if (this.items[i].sellIn < 0) {
+          this.items[i].quality -= 2;
+          // All other items decrease in quality value by one
         } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
+          this.items[i].quality -= 1;
+        }
+        // Block of code corrects any quality levels over 50 and under 0
+        if (this.items[i].quality > 50) {
+          this.items[i].quality = 50;
+        } else if (this.items[i].quality < 0) {
+          this.items[i].quality = 0;
         }
       }
     }
