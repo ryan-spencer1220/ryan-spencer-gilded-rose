718f5d8aa23f9cb0ca1843d5887c2f0581b14447
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-04-18 07:27:13 -0700

    Create and pass all tests for Ragnaros

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -129,7 +129,7 @@ describe("Gilded Rose", function () {
     expect(items[0].quality).toBe(50);
   });
 
-  it("should decrease the sellIn value by one and increase quality by two", () => {
+  it("should decrease the sellIn value by one and increase quality by one", () => {
     const gildedRose = new Shop([new Item("Aged Brie", 8, 49)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe("Aged Brie");
@@ -191,5 +191,75 @@ describe("Gilded Rose", function () {
     expect(items[0].name).toBe("Aged Brie");
     expect(items[0].sellIn).toBe(10);
     expect(items[0].quality).toBe(50);
+  });
+
+  it("should decrease the sellIn value by one", () => {
+    const gildedRose = new Shop([
+      new Item("Backstage passes to a TAFKAL80ETC concert", 4, 50),
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe("Backstage passes to a TAFKAL80ETC concert");
+    expect(items[0].sellIn).toBe(3);
+    expect(items[0].quality).toBe(50);
+  });
+
+  it("should decrease the sellIn value by one and increase the quality value by one", () => {
+    const gildedRose = new Shop([
+      new Item("Backstage passes to a TAFKAL80ETC concert", 4, 49),
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe("Backstage passes to a TAFKAL80ETC concert");
+    expect(items[0].sellIn).toBe(3);
+    expect(items[0].quality).toBe(50);
+  });
+
+  it("should decrease the sellIn value by one and increase the quality value by two", () => {
+    const gildedRose = new Shop([
+      new Item("Backstage passes to a TAFKAL80ETC concert", 4, 48),
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe("Backstage passes to a TAFKAL80ETC concert");
+    expect(items[0].sellIn).toBe(3);
+    expect(items[0].quality).toBe(50);
+  });
+
+  it("should decrease the sellIn value by one increase the quality value by three", () => {
+    const gildedRose = new Shop([
+      new Item("Backstage passes to a TAFKAL80ETC concert", 4, 47),
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe("Backstage passes to a TAFKAL80ETC concert");
+    expect(items[0].sellIn).toBe(3);
+    expect(items[0].quality).toBe(50);
+  });
+
+  it("should maintain the same quality value and sellIn value", () => {
+    const gildedRose = new Shop([
+      new Item("Sulfuras, Hand of Ragnaros", 11, 80),
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe("Sulfuras, Hand of Ragnaros");
+    expect(items[0].sellIn).toBe(11);
+    expect(items[0].quality).toBe(80);
+  });
+
+  it("should maintain the same quality value and sellIn value", () => {
+    const gildedRose = new Shop([
+      new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe("Sulfuras, Hand of Ragnaros");
+    expect(items[0].sellIn).toBe(0);
+    expect(items[0].quality).toBe(80);
+  });
+
+  it("should maintain the same quality value and sellIn value", () => {
+    const gildedRose = new Shop([
+      new Item("Sulfuras, Hand of Ragnaros", -3, 80),
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe("Sulfuras, Hand of Ragnaros");
+    expect(items[0].sellIn).toBe(-3);
+    expect(items[0].quality).toBe(80);
   });
 });
